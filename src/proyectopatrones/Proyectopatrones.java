260741c7bdafe50d6f18d629b8187909bbea342a
codeneomatrix <josuecevedo@gmail.com>
2016-03-21 19:09:30 -0600

    descargando contenido

--- src/proyectopatrones/Proyectopatrones.java
+++ src/proyectopatrones/Proyectopatrones.java
@@ -3,7 +3,9 @@ package proyectopatrones;
 public class Proyectopatrones {
 
     public static void main(String[] args) {
+        Descarga d1 = new Descarga();
         
+        d1.getRSSfromURL("http://codigoprogramacion.com/feed");
     }
     
 }
