9054428c2300b2ff282c5d6ce2ab88fb1b8878c9
codeneomatrix <josuecevedo@gmail.com>
2016-03-21 19:33:44 -0600

    agregando jaunt

--- src/proyectopatrones/Proyectopatrones.java
+++ src/proyectopatrones/Proyectopatrones.java
@@ -1,11 +1,24 @@
 package proyectopatrones;
-
+//import com.jaunt.*;
+//import com.jaunt.component.*;
+//import java.io.*;
 public class Proyectopatrones {
 
     public static void main(String[] args) {
         Descarga d1 = new Descarga();
         
         d1.getRSSfromURL("http://codigoprogramacion.com/feed");
+        
+    
+  /*      
+    try{
+      UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
+      userAgent.visit("http://oracle.com");                        //visit a url   
+      System.out.println(userAgent.doc.innerHTML());               //print the content as HTML
+    }
+    catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+      System.err.println(e);
+    }*/
     }
     
 }
